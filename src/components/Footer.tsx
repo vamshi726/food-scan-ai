b1da6f622af55cde37c4d2b04bf0521a8b0750e8
Vamshi Muluguri <vamshimulgoori2001@gmail.com>
2026-01-01 20:56:29 +0530

    Add Footer component with developer credit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full text-center text-yellow-600 dark:text-yellow-400 py-2 text-xs font-light">
+      Developed by Vamshi Muluguri
+    </footer>
+  );
+};
+
+export default Footer;
